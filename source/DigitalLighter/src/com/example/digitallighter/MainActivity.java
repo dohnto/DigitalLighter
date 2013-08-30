ad0516a273cb663ee142ddf3cc17efc3b17c8dbc
Milos Jovac <milosjovac@gmail.com>
2013-08-30 18:59:03 +0200

    tadadad

--- source/DigitalLighter/src/com/example/digitallighter/MainActivity.java
+++ source/DigitalLighter/src/com/example/digitallighter/MainActivity.java
@@ -3,14 +3,23 @@ package com.example.digitallighter;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main_activity);
 
+		TextView tv;
+		ImageView v;
+
+		tv = new TextView(this);
+
+		tv.setText("Helloooooo!!!!");
+
+		setContentView(tv);
 	}
 
 	@Override
