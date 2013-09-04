361da414f2dd89c3fb0d470d6d40ed4c308e3a58
Milos Jovac <milosjovac@gmail.com>
2013-09-04 13:49:05 +0200

    userStory Done

--- source/DigitalLighter/src/com/example/digitallighter/MainActivity.java
+++ source/DigitalLighter/src/com/example/digitallighter/MainActivity.java
@@ -1,25 +1,30 @@
 package com.example.digitallighter;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.app.Activity;
+import android.graphics.Color;
 import android.view.Menu;
-import android.widget.ImageView;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
+
+	View background;
+	TextView counter;
+	boolean playingSignal = false;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main_activity);
 
-		TextView tv;
-		ImageView v;
+		Button action = (Button) findViewById(R.id.action_button);
+		action.setOnClickListener(this);
+		counter = (TextView) findViewById(R.id.txt_count);
 
-		tv = new TextView(this);
-
-		tv.setText("Helloooooo!!!!");
-		// first commit 
-		setContentView(tv);
 	}
 
 	@Override
@@ -27,6 +32,34 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.action_button:
+			if (!playingSignal) {
+				playingSignal = true;
+				background.setBackgroundColor(Color.WHITE);
+				new CountDownTimer(10000, 1000) {
+
+					public void onTick(long millisUntilFinished) {
+						counter.setText("" + (int) millisUntilFinished / 1000);
+					}
+
+					public void onFinish() {
+						background.setBackgroundColor(Color.BLACK);
+						counter.setText("");
+						playingSignal = false;
+					}
+				}.start();
+			}
+
+			break;
+
+		default:
+			break;
+		}
 	}
 
 }
