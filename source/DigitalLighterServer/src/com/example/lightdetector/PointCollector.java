37977333dec1f16f0f0376106dc8cdd7a40db291
Milos Jovac <milosjovac@gmail.com>
2013-10-03 17:09:42 +0200

    collector

--- source/DigitalLighterServer/src/com/example/lightdetector/PointCollector.java
+++ source/DigitalLighterServer/src/com/example/lightdetector/PointCollector.java
@@ -5,24 +5,26 @@ import java.util.ArrayList;
 import org.opencv.core.Mat;
 import org.opencv.core.Point;
 import org.opencv.core.Scalar;
+import org.opencv.core.Size;
 
 public class PointCollector {
 
-	public static final int NUM_OF_ROWS = 2;
-	public static final int NUM_OF_COLS = 2;
+	public static final int TILE_COUNT_X = 4;
+	public static final int TILE_COUNT_Y = 4;
 	
 	ArrayList<Mat> rgbResources;
 	LightDetector mDetector;
 	Scalar mBGRColor;
-	//TileMapper mMapper;
+	TileMapper mMapper;
 
 
 	public PointCollector(ArrayList<Mat> resOut) {
 		rgbResources = resOut;
 		mDetector = new LightDetector();
 		mBGRColor = new Scalar(0.0, 0.0, 255.00, 0.0);
-		//mMapper = new Mapper(NUM_OF_ROWS,NUM_OF_COLS);
+		mMapper = new TileMapper(TILE_COUNT_X,TILE_COUNT_Y);
 	}
+	
 
 	public void collect() {
 		Thread processThread = new Thread(new Runnable() {
@@ -31,7 +33,8 @@ public class PointCollector {
 			public void run() {
 				for (Mat mat : rgbResources) {
 					ArrayList<Point> points = mDetector.getBlobCoords(mat, mBGRColor);
-					//mMapper.map()
+					Size imgSize = new Size((double)mat.height(), (double) mat.width());
+					mMapper.mapList(imgSize, points);
 					
 					System.out.println("Device is in quadrant:");
 				}
