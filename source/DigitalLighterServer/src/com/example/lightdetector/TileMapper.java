588d21d108729d45cfa602eaadaabf3ec390ef5e
Tomas Dohnalek <dohnto@gmail.com>
2013-10-03 16:55:22 +0200

    Finished implementation of Tilemapper.

--- source/DigitalLighterServer/src/com/example/lightdetector/TileMapper.java
+++ source/DigitalLighterServer/src/com/example/lightdetector/TileMapper.java
@@ -22,12 +22,36 @@ public class TileMapper {
 		this.tilesCountX = tileCountX;
 		this.tilesCountY = tileCountY;
 	}
-
-	public Point map(Point imageSize, Point point) {
-		return new Point();
-	}
 	
 	public ArrayList<Point> mapList(Point imageSize, ArrayList<Point> listOfPoints) {
-		return new ArrayList<Point>();
+		ArrayList<Point> result = new ArrayList<Point>();
+		
+		for (Point point: listOfPoints) {
+			result.add(map(imageSize, point));
+		}
+		
+		return result;
+	}
+
+	public Point map(Point imageSize, Point point) {
+		assert point.x >= 0 && point.x < imageSize.x && point.y >= 0 && point.y < imageSize.y;
+		
+		Point tile = new Point();
+		
+		tile.x = mapToBox(tilesCountX, imageSize.x, point.x);
+		tile.y = mapToBox(tilesCountY, imageSize.y, point.y);
+		
+		return tile;
+	}
+	
+	private int mapToBox(int boxes, int size, int position) {
+		int boxSize = size/boxes; 
+		int sum = 0;
+		int index = 0;
+		while (sum < position) {
+			sum += boxSize;
+			index++;
+		}
+		return index - 1;
 	}
 }
