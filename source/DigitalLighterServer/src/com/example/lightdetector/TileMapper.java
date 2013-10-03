b1dc6d00d1794903d8be694115efc4d9d1a7ab92
Tomas Dohnalek <dohnto@gmail.com>
2013-10-03 17:15:47 +0200

    TileMapper fixed.

--- source/DigitalLighterServer/src/com/example/lightdetector/TileMapper.java
+++ source/DigitalLighterServer/src/com/example/lightdetector/TileMapper.java
@@ -2,18 +2,25 @@ package com.example.lightdetector;
 
 import java.util.ArrayList;
 
-import android.graphics.Point;
+import org.opencv.core.Point;
+import org.opencv.core.Size;
 
+/**
+ * This class maps point from image to given category.
+ * Consider 
+ * @author Tomas Dohnalek
+ *
+ */
 public class TileMapper {
 	
 	private int tilesCountX;
 	private int tilesCountY;
-	
-	
-	public TileMapper(Point tilesCount) {
-		setTileSize(tilesCount.x, tilesCount.y);
-	}
-	
+
+	/**
+	 * Creates TileMapping class that
+	 * @param tilesCountX
+	 * @param tilesCountY
+	 */
 	public TileMapper(int tilesCountX, int tilesCountY) {
 		setTileSize(tilesCountX, tilesCountY);
 	}
@@ -23,7 +30,7 @@ public class TileMapper {
 		this.tilesCountY = tileCountY;
 	}
 	
-	public ArrayList<Point> mapList(Point imageSize, ArrayList<Point> listOfPoints) {
+	public ArrayList<Point> mapList(Size imageSize, ArrayList<Point> listOfPoints) {
 		ArrayList<Point> result = new ArrayList<Point>();
 		
 		for (Point point: listOfPoints) {
@@ -33,25 +40,27 @@ public class TileMapper {
 		return result;
 	}
 
-	public Point map(Point imageSize, Point point) {
-		assert point.x >= 0 && point.x < imageSize.x && point.y >= 0 && point.y < imageSize.y;
-		
+	public Point map(Size imageSize, Point point) {
 		Point tile = new Point();
 		
-		tile.x = mapToBox(tilesCountX, imageSize.x, point.x);
-		tile.y = mapToBox(tilesCountY, imageSize.y, point.y);
+		tile.x = mapToBox(tilesCountX, imageSize.width, point.x);
+		tile.y = mapToBox(tilesCountY, imageSize.height, point.y);
 		
 		return tile;
 	}
 	
-	private int mapToBox(int boxes, int size, int position) {
-		int boxSize = size/boxes; 
-		int sum = 0;
+	private int mapToBox(int boxes, double size, double position) {
+		double boxSize = size/boxes; 
+		double sum = 0;
 		int index = 0;
 		while (sum < position) {
 			sum += boxSize;
 			index++;
 		}
-		return index - 1;
+		
+		if (index > 0)
+			index -= 1; // correction 
+		
+		return index;
 	}
 }
