9211841fff5facb8211b705f3ff916c184e827df
Milos Jovac <milosjovac@gmail.com>
2013-10-03 19:42:56 +0200

    new listener

--- source/DigitalLighterServer/src/com/example/lightdetector/PointCollector.java
+++ source/DigitalLighterServer/src/com/example/lightdetector/PointCollector.java
@@ -7,6 +7,9 @@ import org.opencv.core.Point;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 
+import android.os.Handler;
+import android.os.Message;
+
 public class PointCollector {
 
 	// COLORS TO SEARCH FOR
@@ -16,13 +19,29 @@ public class PointCollector {
 	LightDetector mDetector;
 	TileMapper mMapper;
 
-	public PointCollector(int titleCountX, int titleCountY) {
+	// LISTENER THAT CATCH THE UPDATES
+	PointCollectorListener listener;
+	private Handler mUpdateHandler;
+	PointCollectorUpdate update;
+
+	public PointCollector(int titleCountX, int titleCountY, PointCollectorListener listener) {
 		mDetector = new LightDetector();
 		mBGRColors = new ArrayList<Scalar>();
+		this.listener = listener;
 		mBGRColors.add(new Scalar(0.0, 255.0, 0.00, 0.0)); // green
 		mBGRColors.add(new Scalar(255.0, 0.0, 0.00, 0.0)); // blue
 		mBGRColors.add(new Scalar(0.0, 0.0, 255.00, 0.0)); // red
 		mMapper = new TileMapper(titleCountX, titleCountY);
+
+		// HENDELR GETS MESSAGES FROM BACKGROUND THREADS AND MAKE MODIFICATIONS TO UI
+
+		mUpdateHandler = new Handler() {
+			@Override
+			public void handleMessage(Message msg) {
+
+			}
+
+		};
 	}
 
 	public void collect(final Mat img) {
@@ -33,17 +52,16 @@ public class PointCollector {
 			@Override
 			public void run() {
 
+				update = new PointCollectorUpdate();
+
 				// FIND ALL DEVICES ON IMG
 				for (Scalar color : mBGRColors) {
 					ArrayList<Point> points = mDetector.getBlobCoords(img, color);
 					Size imgSize = new Size((double) img.height(), (double) img.width());
 
-					// GET SCREEN POSITION FOR EVERY DEVICE
+					for (Point p : mMapper.mapList(imgSize, points)) {
 
-					System.out.println("\nCOLOR: " + color.toString());
-					System.out.println("=====================================");
-					for (Point p : mMapper.mapList(imgSize, points))
-						System.out.println("Device in quadrant(" + p.x + "," + p.y + ")");
+					}
 				}
 			}
 		});
