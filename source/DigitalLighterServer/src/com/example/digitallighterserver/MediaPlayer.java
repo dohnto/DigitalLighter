d5d438f5de97b23608e7c41dbd929c00b2bb0cdc
Jan Bednarik <ja.bedna1@gmail.com>
2013-10-17 13:05:51 +0200

    aa]

--- source/DigitalLighterServer/src/com/example/digitallighterserver/MediaPlayer.java
+++ source/DigitalLighterServer/src/com/example/digitallighterserver/MediaPlayer.java
@@ -1,7 +1,25 @@
 package com.example.digitallighterserver;
 
 public class MediaPlayer {	
-	public MediaPlayer(int tilesX, int tilesY, DeviceLocatingStrategy dls) {		
+	
+	private int tilesX;
+	private int tilesY;
+	
+	/**
+	 * Constructor
+	 * @param tilesX number of tiles x axis
+	 * @param tilesY number of tiles y axis
+	 * @param dls DeviceTracker class
+	 */
+	public MediaPlayer(int tilesX, int tilesY, DeviceLocatingStrategy devices, ConnectionService network) {		
+		
+	}
+	
+	/**
+	 * Gets the next frame from ImageMapper and displays it on phones' screens.
+	 * @return Playback finished
+	 */
+	public boolean playNextFrame() {
 		
 	}
 }
