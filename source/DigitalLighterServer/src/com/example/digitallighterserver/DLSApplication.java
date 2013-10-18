e4eb0682a51c17417d9a714ab2e3a7a5c9345397
Tomas Dohnalek <dohnto@gmail.com>
2013-10-18 13:59:42 +0200

    Draft of ImageMapper.

--- source/DigitalLighterServer/src/com/example/digitallighterserver/DLSApplication.java
+++ source/DigitalLighterServer/src/com/example/digitallighterserver/DLSApplication.java
@@ -1,6 +1,7 @@
 package com.example.digitallighterserver;
 
 import android.app.Application;
+import android.content.Context;
 
 public class DLSApplication extends Application {
 	@Override
@@ -8,5 +9,11 @@ public class DLSApplication extends Application {
 		super.onCreate();
 		//Initialize TestFlight with your app token.
 		//sTestFlight.takeOff(this, "0322b950-8564-423a-835d-476531c1ab23");  
+	}
+	
+	static private DLSApplication ctx = new DLSApplication();
+	
+	static public Context getContext() {
+		return ctx.getContext(); 
 	}
 }
