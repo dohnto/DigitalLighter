8cd39f2b9544fb5e9ab46218359e178663b1a1ac
Tomas Dohnalek <dohnto@gmail.com>
2013-10-21 16:52:27 +0200

    Deleted dead code.

--- source/DigitalLighterServer/src/com/example/lightdetector/CameraActivity.java
+++ source/DigitalLighterServer/src/com/example/lightdetector/CameraActivity.java
@@ -56,9 +56,6 @@ public class CameraActivity extends Activity implements CvCameraViewListener2, O
 	DeviceLocatingStrategy dl;
 	MediaPlayer mediaPlayer = null;
 
-	// COLORS
-	ArrayList<String> screenColors = new ArrayList<String>();
-
 	private CameraBridgeViewBase mOpenCvCameraView;
 	private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
 		@Override
@@ -140,10 +137,6 @@ public class CameraActivity extends Activity implements CvCameraViewListener2, O
 	@Override
 	public void onCameraViewStarted(int width, int height) {
 		// TODO Auto-generated method stub
-
-		screenColors.add(ColorManager.BLUE);
-		screenColors.add(ColorManager.GREEN);
-		screenColors.add(ColorManager.RED);
 	}
 
 	@Override
