4a83cd0a7479061bba652e6605aa517f4fd79ba2
Tomas Dohnalek <dohnto@gmail.com>
2013-11-20 22:02:00 +0100

    Header comments.

--- source/DigitalLighterServer/src/com/silentducks/digitallighterserver/network/ConnectionService.java
+++ source/DigitalLighterServer/src/com/silentducks/digitallighterserver/network/ConnectionService.java
@@ -1,3 +1,14 @@
+/**
+  * Digital Ligter
+  * Customer Driven Project - NTNU
+  * 20th November  2013
+  *
+  * @author Jan Bednarik
+  * @author Tomas Dohnalek
+  * @author Milos Jovac
+  * @author Agnethe Soraa
+  */
+
 package com.silentducks.digitallighterserver.network;
 
 import java.net.Socket;
